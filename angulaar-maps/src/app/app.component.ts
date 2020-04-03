30626b44cbec4570ce762ae14cce1358de310105
mattiatosoni <tosoni.mattia@iisgalvanimi.edu.it>
2020-04-03 07:37:11 +0000

    01.1

--- angulaar-maps/src/app/app.component.ts
+++ angulaar-maps/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
 export class AppComponent {
-  title = 'angulaar-maps';
+  title = 'server mappe';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  lng: number = 9.1;
+  lat: number = 45.5;
+
+  icon =  {
+    url:'./assets/img/cat_acrobat.ico',
+    scaledSize: {
+      width: 60,
+      height: 60
+    }
+  };
 }
